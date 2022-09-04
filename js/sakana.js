af79971b0d2c95a8e6a39870406da59df1842d97
ZeaLotSean <zxsean@126.com>
2022-09-04 23:37:05 +0800

    Site updated: 2022-09-04 23:36:47

---
+++ js/sakana.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/sakana"></script>
+<script>
+Sakana.init({
+  el:         '.sakana-box',     // 启动元素 node 或 选择器
+  scale:      .5,                // 缩放倍数
+  canSwitchCharacter: true,      // 允许换角色
+});
+</script>
